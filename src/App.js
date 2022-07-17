681303bb4d78d435f382fa94b1d3ec3954591bac
turovskiy <alexturik@gmail.com>
2022-07-17 14:34:59 +0300

    install reactapp

--- src/App.js
+++ src/App.js
@@ -1,48 +1,24 @@
-import React from 'react';
-import { useState } from "react";
-import Search from "./components/search/search";
-import CurrentWeather from "./components/current-weather/current-weather";
-import Forecast from "./components/forecast/forecast";
-import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
-import MadeWithLove from "./components/ui/Madewithlove";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [currentWeather, setCurrentWeather] = useState(null);
-  const [forecast, setForecast] = useState(null);
-
-  const handleOnSearchChange = (searchData) => {
-    const [lat, lon] = searchData.value.split(" ");
-
-    const currentWeatherFetch = fetch(
-      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-    );
-    const forecastFetch = fetch(
-      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-    );
-
-    Promise.all([currentWeatherFetch, forecastFetch])
-      .then(async (response) => {
-        const weatherResponse = await response[0].json();
-        const forcastResponse = await response[1].json();
-
-        setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        setForecast({ city: searchData.label, ...forcastResponse });
-      })
-      .catch(console.log);
-  };
-
   return (
-    <>
-      <div className="container flex flex-col items-center justify-center w-full h-full mx-auto">
-          <Search onSearchChange={handleOnSearchChange} />
-          {currentWeather && <CurrentWeather data={currentWeather} />}
-          {forecast && <Forecast data={forecast} />}
-          <MadeWithLove/>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    </>
-
   );
 }
 
